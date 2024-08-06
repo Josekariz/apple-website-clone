7a714c038ee50939645f762f2abd606efb45e4e3
Jos <sejokarizz@gmail.com>
2024-08-06 16:30:44 +0300

    add features component

---
+++ src/components/Features.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className="h-full common-padding bg-zinc relativve overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+            <h1 id="features_title"></h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
